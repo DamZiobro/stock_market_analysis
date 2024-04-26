c9a1ff1be9ffa9829ab331fb2eba813a6aaad165
Damian Ziobro <damian@xmementoit.com>
2024-04-26 23:22:09 +0100

    implement fetching data and distribute data to process in parallel in batches

--- stock_market_analysis/steps/fetchData.py
+++ stock_market_analysis/steps/fetchData.py
@@ -1,6 +1,46 @@
-def handler(event, context):
-    print("Hello World from fetchData")
-    return {
-        'statusCode': 200,
-        'body': "Hello World from fetchData"
-    }
+"""Fetching companies codes from CSV file and return as JSON."""
+import csv
+from pathlib import Path
+from typing import List
+
+from pydantic import BaseModel, FilePath
+
+
+class CSVData(BaseModel):
+    """Model of Code<->Name association."""
+
+    Code: str
+    Name: str
+
+
+def read_csv(file_path: FilePath) -> List[CSVData]:
+    """Read a CSV file and converts it to a list of CSVData.
+
+    :param file_path: Path to the CSV file.
+    :return: List of CSVData instances.
+    """
+    data_list = []
+    with Path(file_path).open(newline="", encoding="utf-8") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data_list.append(CSVData(**row))
+    return data_list
+
+
+def handler(event: dict, context: dict) -> list:  # noqa: ARG001
+    """AWS Lambda function to process CSV file to JSON.
+
+    :param event: Lambda event.
+    :param context: Lambda context.
+    :return: JSON response.
+    """
+    #TODO(damian): pass the BATCH_SIZE from serverless.yml vars  # noqa: TD003
+    BATCH_SIZE = 20  # noqa: N806
+
+    pwd = Path(__file__).parent.parent
+    csv_file_path = pwd / "data" / "ftse.csv"
+    data = read_csv(csv_file_path)
+    code_name_records = [item.dict() for item in data]
+    return [
+        code_name_records[i : i + BATCH_SIZE] for i in range(0, len(code_name_records), BATCH_SIZE)
+    ]  # Split data into batches of 20
